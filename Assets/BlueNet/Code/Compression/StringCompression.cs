f5cb1f3ae4650fdce275a2551bfcee69f71a30f0
Lazy_Boy <li0nleo117@gmail.com>
2024-02-13 18:30:22 +0000

    string compress cross thread share fix

--- Assets/BlueNet/Code/Compression/StringCompression.cs
+++ Assets/BlueNet/Code/Compression/StringCompression.cs
@@ -10,71 +10,75 @@ namespace BlueNet.Compression
     public class StringCompressor : CompressionBase
     {
         const int DuplicateStringLengthThreshold = 3;
-        StringBuilder stringBuilder;
-        StringBuilder current;
+        StringBuilder CompressStringBuilder;
+        StringBuilder CompressCurrent;
+        StringBuilder DeCompressStringBuilder;
+        StringBuilder DeCompressCurrent;
         public StringCompressor()
         {
-            stringBuilder = new StringBuilder(500);
-            current = new StringBuilder(50);
+            CompressStringBuilder = new StringBuilder(500);
+            CompressCurrent = new StringBuilder(50);
+            DeCompressStringBuilder = new StringBuilder(500);
+            DeCompressCurrent = new StringBuilder(50);
         }
         public override byte[] Compress(string value)
         {
-            stringBuilder.Clear();
+            CompressStringBuilder.Clear();
 
             //identified duplicates and the replacement
             var duplicateArgs = new Dictionary<string, string>();
             int KeyIndex = 0;
             //the current value being read
-            StringBuilder current = new StringBuilder();
+            CompressCurrent.Clear();
             for (int c = 0; c < value.Length; c++)
             {
                 //is this the end of a value
                 if (value[c] == ',' || value[c] == '|')
                 {
                     //is the value large enough to compress
-                    if (current.Length > DuplicateStringLengthThreshold)
+                    if (CompressCurrent.Length > DuplicateStringLengthThreshold)
                     {
-                        string currentValue = current.ToString();
+                        string currentValue = CompressCurrent.ToString();
                         if (duplicateArgs.ContainsKey(currentValue))
                         {
                             //this value was alread a duplicate so replace with the key
-                            stringBuilder.Append(duplicateArgs[currentValue]);
+                            CompressStringBuilder.Append(duplicateArgs[currentValue]);
                         }
                         else
                         {
                             //this value is the first of its kind, set as a value 
-                            stringBuilder.Append("^").Append(currentValue).Append("^");
+                            CompressStringBuilder.Append("^").Append(currentValue).Append("^");
                             duplicateArgs.Add(currentValue, "^" + KeyIndex.ToString() + "^");
                             KeyIndex++;
                         }
                     }
                     else
                     {
-                        stringBuilder.Append(current);
+                        CompressStringBuilder.Append(CompressCurrent);
                     }
-                    current.Clear();
-                    stringBuilder.Append(value[c]);
+                    CompressCurrent.Clear();
+                    CompressStringBuilder.Append(value[c]);
                 }
                 else
                 {
-                    current.Append(value[c]);
+                    CompressCurrent.Append(value[c]);
                 }
 
             }
 
             //call base method to convert string to byte array
-            return base.Compress(stringBuilder.ToString());
+            return base.Compress(CompressStringBuilder.ToString());
         }
         public override string DeCompress(byte[] bytesvalue)
         {
             //get string from byte value
             string value = base.DeCompress(bytesvalue);
-            stringBuilder.Clear();
+            DeCompressStringBuilder.Clear();
 
             //identified duplicates
             var duplicateArgs = new Dictionary<string, string>();
             int keyIndex = 0;
-            current.Clear();
+            DeCompressCurrent.Clear();
             bool gettingCompVal = false;
             for (int c = 0; c < value.Length; c++)
             {
@@ -85,18 +89,18 @@ namespace BlueNet.Compression
                     {
                         gettingCompVal = false;
 
-                        string currentValue = current.ToString();
-                        current.Clear();
+                        string currentValue = DeCompressCurrent.ToString();
+                        DeCompressCurrent.Clear();
                         //is the duplicate value a key
                         if (duplicateArgs.TryGetValue(currentValue, out string key))
                         {
                             //replace value with correct value
-                            stringBuilder.Append(key);
+                            DeCompressStringBuilder.Append(key);
                         }
                         else
                         {
                             //is the value for the keys
-                            stringBuilder.Append(currentValue);
+                            DeCompressStringBuilder.Append(currentValue);
                             //add to dictonary for keys to decode
                             duplicateArgs.Add(keyIndex.ToString(), currentValue);
                             keyIndex++;
@@ -106,7 +110,7 @@ namespace BlueNet.Compression
                     }
                     else
                     {
-                        current.Append(value[c]);
+                        DeCompressCurrent.Append(value[c]);
                     }
                 }
                 else
@@ -117,12 +121,12 @@ namespace BlueNet.Compression
                         gettingCompVal = true;
                         continue;
                     }
-                    stringBuilder.Append(value[c]);
+                    DeCompressStringBuilder.Append(value[c]);
                 }
 
             }
             //return message as a string
-            return stringBuilder.ToString();
+            return DeCompressStringBuilder.ToString();
         }
     }
 }
